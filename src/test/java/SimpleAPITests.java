28fc5c5f08747d6337b7f8f868b0476ff1a18622
Sergei Pershin <sergei.pershin@live.com>
2020-12-06 19:43:19 +0200

    Additional Test Added

--- src/test/java/SimpleAPITests.java
+++ src/test/java/SimpleAPITests.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
 import static io.restassured.RestAssured.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class SimpleAPITests {
 
@@ -10,6 +12,20 @@ public class SimpleAPITests {
         get("https://demoqa.com/BookStore/v1/Books")
                 .then()
                 .body("books[0].title", is("Git Pocket Guide"));
+    }
+
+    @Test
+    void positiveTest() {
+        int a = 3;
+        int b = 4;
+        assertEquals(a + b, 7);
+    }
+
+    @Test
+    void negativeTest() {
+        int a = 3;
+        int b = 4;
+        assertNotEquals(a + b, 8);
     }
 
 }
